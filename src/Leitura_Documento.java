eb60e027f6814934b83958482e3d75ed72f71223
Michael Barbosa <michaelmelobarbosa@gmal.com>
2026-03-13 16:05:22 -0300

    comentários removidos para deixar codigo limpo

--- src/Leitura_Documento.java
+++ src/Leitura_Documento.java
@@ -6,30 +6,18 @@ import java.util.ArrayList;
 public class Leitura_Documento {
     public static void main(String[] args) {
 
-        String path = "Autentificador_Digital.txt";
+        String path = "AutenticadorDigital.txt";
 
         try {
             BufferedReader ler_Arquivo = new BufferedReader(new FileReader(path));
-
-            // VARIAVEL DO TIPO STRING QUE VAI RECEBER AS LINHAS
             String linha;
-
-            // A LISTA DINAMICA QUE VAI RECEBER OS DADOS
             ArrayList<String> palavras = new ArrayList<String>();
 
-            // O LAÇO DE REPETIÇÃO QUE VAI LER TODO O ARQUIVO .TXT, ADICIONANDO CADA LINHA
-            // LIDA, NA VARIALVEL LINHA
             while ((linha = ler_Arquivo.readLine()) != null) {
-                // System.out.println("Linha: " + i + "\n" + linha + "\n");
-                // ARRAY DE STRING QUE VAI RECEBER AS PALAVRAS SEPARADAS DE CADA LINHA
                 String[] palavas_linha = linha.split(" ");
-
                 for (String indice_palavra : palavas_linha) {
                     palavras.add(indice_palavra);
                 }
-
-                // System.out.println("Linha" + " " + i + "\n" + linha + "\n");
-                // i++;
             }
             System.out.println(palavras);
             ler_Arquivo.close();
