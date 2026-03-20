cc9e9eecb9bcbf783380a35f1286da5f63df0d2f
Michael Barbosa <michaelmelobarbosa@gmal.com>
2026-03-20 18:34:53 -0300

    removidos comentarios na classe PilhaArvore

--- src/estruturas/PilhaArvore.java
+++ src/estruturas/PilhaArvore.java
@@ -4,10 +4,8 @@ import java.util.EmptyStackException;
 
 public class PilhaArvore {
 
-    // topo da pilha
     private NoPilha topo;
 
-    //Nó da pilha, guarda uma árvore
     private class NoPilha {
         Arvore dado;
         NoPilha proximo;
@@ -21,17 +19,17 @@ public class PilhaArvore {
         topo = null;
     }
 
-    //Coloca uma árvore na pilha
+
     public void empilhar(Arvore arvoreDaLinha) {
         NoPilha novoElemento = new NoPilha(arvoreDaLinha);
         novoElemento.proximo = topo;
         topo = novoElemento;
     }
 
-    //Remove o elemento do topo
+
     public Arvore desempilhar() {
         if(estaVazia()){
-            throw new EmptyStackException(); //dispara o erro, caso a pilha esteja vazia e tentarem desempilhar
+            throw new EmptyStackException();
         }
 
         Arvore removida = topo.dado;
