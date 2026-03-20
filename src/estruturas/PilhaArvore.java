faafcdd63e4949e609fba79b791222d8206c63bd
Beatriz <anabeatriz.abreu123@gmail.com>
2026-03-20 11:04:18 -0300

    implementando a estrutura de pilha

--- src/estruturas/PilhaArvore.java
+++ src/estruturas/PilhaArvore.java
@@ -1,10 +1,46 @@
 package estruturas;
 
-import java.util.Stack;
+import java.util.EmptyStackException;
 
 public class PilhaArvore {
 
-    public Stack<Arvore> pilhaArvores;
+    // topo da pilha
+    private NoPilha topo;
 
+    //Nó da pilha, guarda uma árvore
+    private class NoPilha {
+        Arvore dado;
+        NoPilha proximo;
 
+        NoPilha(Arvore dado) {
+            this.dado = dado;
+        }
+    }
+
+    public PilhaArvore() {
+        topo = null;
+    }
+
+    //Coloca uma árvore na pilha
+    public void empilhar(Arvore arvoreDaLinha) {
+        NoPilha novoElemento = new NoPilha(arvoreDaLinha);
+        novoElemento.proximo = topo;
+        topo = novoElemento;
+    }
+
+    //Remove o elemento do topo
+    public Arvore desempilhar() {
+        if(estaVazia()){
+            throw new EmptyStackException(); //dispara o erro, caso a pilha esteja vazia e tentarem desempilhar
+        }
+
+        Arvore removida = topo.dado;
+        topo = topo.proximo;
+
+        return removida;
+    }
+
+    public boolean estaVazia(){
+        return topo == null;
+    }
 }
