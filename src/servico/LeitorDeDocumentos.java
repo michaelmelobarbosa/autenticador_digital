0512a9f07bafce0aa247f99be1e39dbcc144eecb
Michael Barbosa <michaelmelobarbosa@gmal.com>
2026-03-21 11:47:19 -0300

    refatorações nos imports da classe LeitorDeDocumentos

--- src/servico/LeitorDeDocumentos.java
+++ src/servico/LeitorDeDocumentos.java
@@ -7,8 +7,6 @@ import estruturas.NoArvore;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class LeitorDeDocumentos {
 
